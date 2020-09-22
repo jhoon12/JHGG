f966a763e0c306fcb221a1b9520155a5f05577c7
parkjaehoon <yukihoon12@naver.com>
2020-09-22 23:47:10 +0900

    JHGG 통신

---
+++ jhgg/src/API.js
@@ -0,0 +1,3 @@
+
+const BaseKey = "RGAPI-37b6de4a-b8c8-4eea-b243-148b12a64b2e";
+export default BaseKey;
--- jhgg/src/SearchUser.js
+++ jhgg/src/SearchUser.js
@@ -1,6 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import BaseKey from "./API";
+import axios from "axios";
+const SearchUser = (props) => {
+  const [userData, setUserData] = useState(null);
+  const [userLeagueData, setUserLeagueData] = useState(null);
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(
+          `/lol/summoner/v4/summoners/by-name/${props.match.params.userName}`,
+          {
+            headers: {
+              "X-Riot-Token": BaseKey,
+            },
+          }
+        );
+        setUserData(data);
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+  useEffect(()=>{
+    (async()=>{
+      try{
+        const res = await axios.get(`/lol/league/v4/entries/by-summoner/${userData.id}`,
+        {
+          headers:{
+            "X-Riot-Token": BaseKey,
+          }
+        }
+        );
+        setUserLeagueData(res.data);
+        console.log(res);
+      }catch(err){
+        console.log(err);
+      }
+    })()
+  },[userData])
 
-const SearchUser = (props)=>{
-    return(<div>{props.match.params.userName}</div>)
-}
+
+  return (
+    <>
+      {userData && (
+        <>
+        <img
+          src={`http://ddragon.leagueoflegends.com/cdn/10.19.1/img/profileicon/${userData.profileIconId}.png`}
+        />
+        {/* <img src={`opgg-static.akamaized.net/images/medals/${}.png?image=q_auto&v=1}/> */}
+        </>
+      )}
+    </>
+  );
+};
 export default SearchUser;
