a37517019a5080ea679a1fb3a40bea74577541d0
parkjaehoon <yukihoon12@naver.com>
2020-09-23 15:23:36 +0900

    티어받아오기

--- jhgg/src/SearchUser.js
+++ jhgg/src/SearchUser.js
@@ -4,6 +4,8 @@ import axios from "axios";
 const SearchUser = (props) => {
   const [userData, setUserData] = useState(null);
   const [userLeagueData, setUserLeagueData] = useState(null);
+  const [userTier, setUserTier] = useState(null);
+  let src;
   useEffect(() => {
     (async () => {
       try {
@@ -16,41 +18,58 @@ const SearchUser = (props) => {
           }
         );
         setUserData(data);
-        console.log(data);
+        
       } catch (err) {
         console.log(err);
       }
     })();
   }, []);
-  useEffect(()=>{
-    (async()=>{
-      try{
-        const res = await axios.get(`/lol/league/v4/entries/by-summoner/${userData.id}`,
-        {
-          headers:{
-            "X-Riot-Token": BaseKey,
+  useEffect(() => {
+    (async () => {
+      try {
+      
+        const res = await axios.get(
+          `/lol/league/v4/entries/by-summoner/${userData.id}`,
+          {
+            headers: {
+              "X-Riot-Token": BaseKey,
+            },
+          }
+        );
+        const ReArab = () =>{
+          switch(res.data[0].rank){
+            case "I" :
+              return(1);
+            case "II":
+              return(2);
+            case "III":
+              return(3);
+            case "IV":
+              return(4);
           }
         }
-        );
-        setUserLeagueData(res.data);
-        console.log(res);
-      }catch(err){
+        setUserLeagueData(res);
+        console.log(res.data);
+        setUserTier(ReArab());
+      } catch (err) {
         console.log(err);
       }
-    })()
-  },[userData])
-
-
+    })();
+  }, [userData]);
   return (
     <>
       {userData && (
-        <>
         <img
           src={`http://ddragon.leagueoflegends.com/cdn/10.19.1/img/profileicon/${userData.profileIconId}.png`}
         />
-        {/* <img src={`opgg-static.akamaized.net/images/medals/${}.png?image=q_auto&v=1}/> */}
-        </>
       )}
+      {userLeagueData&&(
+          <>
+          <img src={`https://opgg-static.akamaized.net/images/medals/${userLeagueData.data[0].tier}_${userTier}.png?image=q_auto&v=1`}/>
+          <img src={`https://opgg-static.akamaized.net/images/medals/${userLeagueData.data[1].tier}_${userTier}.png?image=q_auto&v=1`}/>
+          </>
+      )
+      }
     </>
   );
 };
